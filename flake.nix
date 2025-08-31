b83702907ff974de93236c186df6d199f71938f1
Zhi-Qiang Lei <zhiqiang.lei@gmail.com>
2025-08-31 10:31:42 +0800

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,99 @@
+{
+  description = "R for Quants";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    traderrr = {
+      url = "github:siegfried/traderrr";
+      flake = false;
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      traderrr,
+      ...
+    }:
+    let
+      supportedSystems = [
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+          R = pkgs.rWrapper.override {
+            packages = [
+              (pkgs.callPackage traderrr { })
+              (pkgs.rPackages.lightgbm.overrideAttrs (old: {
+                buildInputs = old.buildInputs ++ [ pkgs.llvmPackages.openmp ];
+              }))
+              (pkgs.rPackages.xgboost.overrideAttrs (old: {
+                buildInputs = old.buildInputs ++ [ pkgs.llvmPackages.openmp ];
+              }))
+            ]
+            ++ (with pkgs.rPackages; [
+              dplyr
+              dbplyr
+              DBI
+              RPostgres
+              readr
+              lubridate
+              tidyr
+              stringr
+              purrr
+              workflows
+              ranger
+              tune
+              yardstick
+              recipes
+              dials
+              parsnip
+              butcher
+              optparse
+              hardhat
+              future
+              doFuture
+              bonsai
+              janitor
+              textrecipes
+              glmnet
+              vip
+            ]);
+          };
+        in
+        {
+          inherit R;
+
+          default = self.packages.${pkgs.system}.R;
+        }
+      );
+
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              self.packages.${pkgs.system}.R
+              pkgs.curl
+            ];
+            shellHook = ''
+              R
+            '';
+          };
+        }
+      );
+
+    };
+}
